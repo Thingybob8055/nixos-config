ff9ebad1091c4a48b447c07e62655dc30cf56826
Akshay Gopinath <akshay8055.007@gmail.com>
2026-04-16 18:04:34 +0100

    pop shell try floating window exceptions

--- framework/home.nix
+++ framework/home.nix
@@ -120,9 +120,23 @@
 
   home.sessionPath = [ "/home/akshay/.local/share/yabridge" ];
 
+  home.file.".config/pop-shell/config.json".text = builtins.toJSON {
+  float = [
+    {
+      class = "pop-shell-example";
+      title = "pop-shell-example";
+    }
+    {
+      title = "Proton VPN";  # adjust if needed
+    }
+  ];
+  skiptaskbarhidden = [];
+  log_on_focus = false;
+ };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  #home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -133,7 +147,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  #};
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
