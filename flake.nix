ddd09df459fab01b4b975893f17ee2e8edb222fe
Akshay Gopinath <akshay8055.007@gmail.com>
2026-05-24 17:08:51 +0100

    add hyprsplit to flake and update stable to 25.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "nixpkgs/nixos-25.11";
 
     #nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
 
@@ -13,6 +13,8 @@
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    hyprsplit.url = "github:shezdy/hyprsplit";
 
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
