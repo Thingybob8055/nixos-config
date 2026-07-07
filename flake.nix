bd16994feb9f9a95596abcabe7b6925a24854027
Akshay Gopinath <akshay8055.007@gmail.com>
2026-07-08 00:13:47 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
    # minegrub-theme.url = "github:Lxtharia/minegrub-theme";
 
     noctalia = {
-        url = "github:noctalia-dev/noctalia-shell";
+        url = "github:noctalia-dev/noctalia/legacy-v4";
         inputs.nixpkgs.follows = "nixpkgs";
       };
 
